6bb2510f56e622aefe54be118877b9587604d1f5
Andrei Radulescu <andi.radulescu@gmail.com>
2024-02-23 20:27:10 +0200

    fix for showOpenFilePicker

--- src/QDL/utils.js
+++ src/QDL/utils.js
@@ -59,9 +59,79 @@ export function compareStringToBytes(compareString, array) {
   return compareString == tArray;
 }
 
+const openFileOrFiles = async (multiple = false) => {
+  // Feature detection. The API needs to be supported
+  // and the app not run in an iframe.
+  const supportsFileSystemAccess =
+    "showOpenFilePicker" in window &&
+    (() => {
+      try {
+        return window.self === window.top;
+      } catch {
+        return false;
+      }
+    })();
+  // If the File System Access API is supported…
+  if (supportsFileSystemAccess) {
+    let fileOrFiles = undefined;
+    try {
+      // Show the file picker, optionally allowing multiple files.
+      const handles = await showOpenFilePicker({ multiple });
+      // Only one file is requested.
+      if (!multiple) {
+        // Add the `FileSystemFileHandle` as `.handle`.
+        fileOrFiles = await handles[0].getFile();
+        fileOrFiles.handle = handles[0];
+      } else {
+        fileOrFiles = await Promise.all(
+          handles.map(async (handle) => {
+            const file = await handle.getFile();
+            // Add the `FileSystemFileHandle` as `.handle`.
+            file.handle = handle;
+            return file;
+          })
+        );
+      }
+    } catch (err) {
+      // Fail silently if the user has simply canceled the dialog.
+      if (err.name !== 'AbortError') {
+        console.error(err.name, err.message);
+      }
+    }
+    return fileOrFiles;
+  }
+  // Fallback if the File System Access API is not supported.
+  return new Promise((resolve) => {
+    // Append a new `<input type="file" multiple? />` and hide it.
+    const input = document.createElement('input');
+    input.style.display = 'none';
+    input.type = 'file';
+    document.body.append(input);
+    if (multiple) {
+      input.multiple = true;
+    }
+    // The `change` event fires when the user interacts with the dialog.
+    input.addEventListener('change', () => {
+      // Remove the `<input type="file" multiple? />` again from the DOM.
+      input.remove();
+      // If no files were selected, return.
+      if (!input.files) {
+        return;
+      }
+      // Return all files or just one file.
+      resolve(multiple ? Array.from(input.files) : input.files[0]);
+    });
+    // Show the picker.
+    if ('showPicker' in HTMLInputElement.prototype) {
+      input.showPicker();
+    } else {
+      input.click();
+    }
+  });
+};
+
 export async function loadFileFromLocal() {
-  const [fileHandle] = await window.showOpenFilePicker();
-  const blob = await fileHandle.getFile();
+  const blob = await openFileOrFiles();
   return new Promise((resolve, reject) => {
     let reader = new FileReader();
     reader.onload = () => {
